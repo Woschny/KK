b5d62b64487450724f1c73ee48b78f58b22079a1
Woschny <92034289+Woschny@users.noreply.github.com>
2023-05-06 21:04:20 +0200

    additional videos + end click fix

--- index.js
+++ index.js
@@ -7,20 +7,22 @@ function hdlLoad() {
     var debugbtn = document.getElementById("debug");
     var monImg = document.getElementById("MoniImg");
     var ctx = canvas.getContext("2d");
-    var videoInt = 0;
+    var videoInt = 13;
     var video = document.getElementById("video");
-    var sourceString = ["T1.mp4", "T2.mp4", "T3.mp4", "T4.mp4", "T5.mp4", "T6.mp4", "T7.mp4", "T8.mp4", "T9.mp4", "T10.mp4", "T11.mp4", "T12.mp4", "T13.mp4"];
+    var sourceString = ["T1.mp4", "T2.mp4", "T3.mp4", "T4.mp4", "T5.mp4", "T6.mp4", "T7.mp4", "T8.mp4", "T9.mp4", "T10.mp4", "T11.mp4", "T12.mp4", "T13.mp4", "T14.mp4", "T15.mp4"];
     var hitboxLocation = [{ x: 660, y: 780, s: 100 }, { x: -100, y: -100, s: 100 }, { x: 820, y: 540, s: 200 }, { x: -100, y: -100, s: 100 }, { x: 820, y: 540, s: 200 },
         { x: -100, y: -100, s: 100 }, { x: 830, y: 40, s: 800 }, { x: -100, y: -100, s: 100 }, { x: 1080, y: 630, s: 260 }, { x: -100, y: -100, s: 100 },
-        { x: 0, y: 0, s: 2000 }, { x: -100, y: -100, s: 100 }, { x: 520, y: 530, s: 200 }];
-    //  1-90, 90-270, 272-362, 362-460, 560-550, 550-1100, 1100-1190, 1190-2000, 2000-2090, 2090-2300, 2300-2390, 2390-3000, 3000-3090
-    //   t1cy  t2cy    t3ev      t4ev    t5ev     t6ev       t7ev        t8ev       t9ev       t10ev     t11ev      t12ev      t13ev
+        { x: 0, y: 0, s: 2000 }, { x: -100, y: -100, s: 100 }, { x: 520, y: 530, s: 200 }, { x: -100, y: -100, s: 100 }, { x: 1110, y: 550, s: 150 }];
+    //  1-90, 90-270, 272-362, 362-460, 460-550, 550-1100, 1100-1190, 1190-2000, 2000-2090, 2090-2300, 2300-2390, 2390-3000, 3000-3090, 3090-3650, 3650-3740, 
+    //   bt1cy  bt2cy    bt3ev   bt4ev    t5ev     t6ev       bt7ev      bt8ev     bt9ev      bt10ev     bt11ev     bt12ev     bt13ev     bt14ev    bt15ev
     var hitbox = [hitboxLocation[0].x, hitboxLocation[0].y, hitboxLocation[0].s, hitboxLocation[0].s];
     var nextVideo = false;
     var doOneThanSkip = false;
     var scaleFactor = 4.55;
     var debugMode = false;
     var interval;
+    var x;
+    var y;
     playbtn.addEventListener("click", function () { startVideo(), playbtn.style.visibility = "hidden", monImg.src = "./Assets/MonitorBack.png"; });
     debugbtn.addEventListener("click", toggleDebugMode);
     canvas.addEventListener("click", function (e) { return action(getCursorPosition(canvas, e)); });
@@ -30,7 +32,6 @@ function hdlLoad() {
     canvas.style.width = canvas.width * (scaleFactor / 10) + "px";
     function action(vector) {
         console.log(vector);
-        console.log(hitbox);
         if (nextVideo == false && vector[0] >= hitbox[0] && vector[0] <= hitbox[0] + hitbox[2] && vector[1] >= hitbox[1] && vector[1] <= hitbox[1] + hitbox[3]) {
             console.log("hit");
             nextVideo = true;
@@ -45,7 +46,7 @@ function hdlLoad() {
         }
     }
     function hoverVis(vector) {
-        if (nextVideo == false && vector[0] >= hitbox[0] && vector[0] <= hitbox[0] + hitbox[2] && vector[1] >= hitbox[1] && vector[1] <= hitbox[1] + hitbox[3]) {
+        if (vector[0] >= hitbox[0] && vector[0] <= hitbox[0] + hitbox[2] && vector[1] >= hitbox[1] && vector[1] <= hitbox[1] + hitbox[3]) {
             canvas.style.cursor = "pointer";
         }
         else {
@@ -73,22 +74,30 @@ function hdlLoad() {
                 pauseVideo();
                 if (doOneThanSkip == true) {
                     videoInt++;
-                    video.src = "./Assets/" + sourceString[videoInt];
-                    hitbox[0] = hitboxLocation[videoInt].x;
-                    hitbox[1] = hitboxLocation[videoInt].y;
-                    hitbox[2] = hitboxLocation[videoInt].s;
-                    hitbox[3] = hitbox[2];
+                    console.log(videoInt, sourceString.length);
+                    if (videoInt < sourceString.length) {
+                        video.src = "./Assets/" + sourceString[videoInt];
+                        hitbox[0] = hitboxLocation[videoInt].x;
+                        hitbox[1] = hitboxLocation[videoInt].y;
+                        hitbox[2] = hitboxLocation[videoInt].s;
+                        hitbox[3] = hitbox[2];
+                    }
+                    hoverVis([x, y]);
                     doOneThanSkip = false;
                 }
                 else {
                     console.log("finish");
                     if (nextVideo == true) {
                         videoInt++;
-                        video.src = "./Assets/" + sourceString[videoInt];
-                        hitbox[0] = hitboxLocation[videoInt].x;
-                        hitbox[1] = hitboxLocation[videoInt].y;
-                        hitbox[2] = hitboxLocation[videoInt].s;
-                        hitbox[3] = hitbox[2];
+                        console.log(videoInt, sourceString.length);
+                        if (videoInt < sourceString.length) {
+                            video.src = "./Assets/" + sourceString[videoInt];
+                            hitbox[0] = hitboxLocation[videoInt].x;
+                            hitbox[1] = hitboxLocation[videoInt].y;
+                            hitbox[2] = hitboxLocation[videoInt].s;
+                            hitbox[3] = hitbox[2];
+                        }
+                        hoverVis([x, y]);
                         startVideo();
                         nextVideo = false;
                         doOneThanSkip = true;
@@ -100,8 +109,8 @@ function hdlLoad() {
     });
     function getCursorPosition(canvas, event) {
         var rect = canvas.getBoundingClientRect();
-        var x = (event.clientX - rect.left) * (10 / scaleFactor);
-        var y = (event.clientY - rect.top) * (10 / scaleFactor);
+        x = (event.clientX - rect.left) * (10 / scaleFactor);
+        y = (event.clientY - rect.top) * (10 / scaleFactor);
         return [x, y];
     }
 }
--- index.ts
+++ index.ts
@@ -8,14 +8,14 @@ const playbtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("p
 const debugbtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("debug");
 const monImg: HTMLImageElement = <HTMLImageElement>document.getElementById("MoniImg");
 const ctx: CanvasRenderingContext2D    = canvas.getContext("2d");
-let videoInt: number = 0;
+let videoInt: number = 13;
 let video: HTMLVideoElement  = <HTMLVideoElement>document.getElementById("video");
-let sourceString: string[] = ["T1.mp4", "T2.mp4", "T3.mp4", "T4.mp4", "T5.mp4", "T6.mp4", "T7.mp4", "T8.mp4", "T9.mp4", "T10.mp4", "T11.mp4", "T12.mp4", "T13.mp4"];
+let sourceString: string[] = ["T1.mp4", "T2.mp4", "T3.mp4", "T4.mp4", "T5.mp4", "T6.mp4", "T7.mp4", "T8.mp4", "T9.mp4", "T10.mp4", "T11.mp4", "T12.mp4", "T13.mp4", "T14.mp4", "T15.mp4"];
 let hitboxLocation: Vector[] = [{x: 660, y: 780, s: 100}, {x: -100, y: -100, s: 100}, {x: 820, y: 540, s: 200}, {x: -100, y: -100, s: 100}, {x: 820, y: 540, s: 200}, 
                                 {x: -100, y: -100, s: 100}, {x: 830, y: 40, s: 800}, {x: -100, y: -100, s: 100}, {x: 1080, y: 630, s: 260}, {x: -100, y: -100, s: 100},
-                                {x: 0, y: 0, s: 2000}, {x: -100, y: -100, s: 100}, {x: 520, y: 530, s: 200}];
-//  1-90, 90-270, 272-362, 362-460, 560-550, 550-1100, 1100-1190, 1190-2000, 2000-2090, 2090-2300, 2300-2390, 2390-3000, 3000-3090
-//   t1cy  t2cy    t3ev      t4ev    t5ev     t6ev       t7ev        t8ev       t9ev       t10ev     t11ev      t12ev      t13ev
+                                {x: 0, y: 0, s: 2000}, {x: -100, y: -100, s: 100}, {x: 520, y: 530, s: 200}, {x: -100, y: -100, s: 100}, {x: 1110, y: 550, s: 150}];
+//  1-90, 90-270, 272-362, 362-460, 460-550, 550-1100, 1100-1190, 1190-2000, 2000-2090, 2090-2300, 2300-2390, 2390-3000, 3000-3090, 3090-3650, 3650-3740, 
+//   bt1cy  bt2cy    bt3ev   bt4ev    t5ev     t6ev       bt7ev      bt8ev     bt9ev      bt10ev     bt11ev     bt12ev     bt13ev     bt14ev    bt15ev
 let hitbox: number[] = [hitboxLocation[0].x, hitboxLocation[0].y, hitboxLocation[0].s, hitboxLocation[0].s];
 interface Vector {
     x: number;
@@ -29,6 +29,9 @@ let scaleFactor: number = 4.55;
 let debugMode: boolean = false;
 let interval: number;
 
+let x: number;
+let y: number;
+
 playbtn.addEventListener("click", () => {startVideo(), playbtn.style.visibility = "hidden", monImg.src = "./Assets/MonitorBack.png";});
 debugbtn.addEventListener("click", toggleDebugMode);
 canvas.addEventListener("click", (e) => action(getCursorPosition(canvas, e)));
@@ -40,7 +43,6 @@ canvas.style.width = canvas.width*(scaleFactor/10) + "px";
 
 function action(vector: number[]): void {
     console.log(vector);
-    console.log(hitbox);
     if (nextVideo == false && vector[0] >= hitbox[0] && vector[0] <= hitbox[0] + hitbox[2] && vector[1] >= hitbox[1] && vector[1] <= hitbox[1] + hitbox[3]){
         console.log("hit");
         nextVideo = true;
@@ -57,7 +59,7 @@ function toggleDebugMode(): void {
 }
 
 function hoverVis(vector: number[]): void {
-    if (nextVideo == false && vector[0] >= hitbox[0] && vector[0] <= hitbox[0] + hitbox[2] && vector[1] >= hitbox[1] && vector[1] <= hitbox[1] + hitbox[3]){
+    if (vector[0] >= hitbox[0] && vector[0] <= hitbox[0] + hitbox[2] && vector[1] >= hitbox[1] && vector[1] <= hitbox[1] + hitbox[3]){
     canvas.style.cursor = "pointer";
     }
     else {
@@ -88,22 +90,31 @@ video.addEventListener("play", function (): void {
             pauseVideo();
             if (doOneThanSkip == true){
                 videoInt++;
+                console.log(videoInt, sourceString.length);
+                
+                if(videoInt < sourceString.length) {
                 video.src = "./Assets/" + sourceString[videoInt];
                 hitbox[0] = hitboxLocation[videoInt].x;
                 hitbox[1] = hitboxLocation[videoInt].y;
                 hitbox[2] = hitboxLocation[videoInt].s;
                 hitbox[3] = hitbox[2];
+                }
+                hoverVis([x,y]);
                 doOneThanSkip = false;
                 }
             else {
                 console.log("finish");
                 if (nextVideo == true) {
                 videoInt ++;
+                console.log(videoInt, sourceString.length);
+                if(videoInt < sourceString.length) {
                 video.src = "./Assets/" + sourceString[videoInt];
                 hitbox[0] = hitboxLocation[videoInt].x;
                 hitbox[1] = hitboxLocation[videoInt].y;
                 hitbox[2] = hitboxLocation[videoInt].s;
                 hitbox[3] = hitbox[2];
+                }
+                hoverVis([x,y]);
                 startVideo();
                 nextVideo = false;
                 doOneThanSkip = true;
@@ -117,8 +128,8 @@ video.addEventListener("play", function (): void {
 
 function getCursorPosition(canvas: HTMLCanvasElement, event: MouseEvent): number[] {
     const rect: ClientRect = canvas.getBoundingClientRect();
-    let x: number = (event.clientX - rect.left)*(10/scaleFactor);
-    let y: number = (event.clientY - rect.top)*(10/scaleFactor);
+    x = (event.clientX - rect.left)*(10/scaleFactor);
+    y = (event.clientY - rect.top)*(10/scaleFactor);
     return[x, y];
 }
 
