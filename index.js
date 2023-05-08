c162066b35b3d897b9e389571ff4f4170280a153
Woschny <92034289+Woschny@users.noreply.github.com>
2023-05-08 13:19:56 +0200

    Finish blockout

--- index.js
+++ index.js
@@ -10,17 +10,25 @@ function hdlLoad() {
     var videoInt = 0;
     var video = document.getElementById("video");
     var sourceString = ["T1.mp4", "T2.mp4", "T3.mp4", "T4.mp4", "T5.mp4", "T6.mp4", "T7.mp4", "T8.mp4", "T9.mp4", "T10.mp4", "T11.mp4", "T12.mp4",
-        "T13.mp4", "T14.mp4", "T15.mp4", "T16.mp4", "T17.mp4"];
-    var hitboxLocation = [{ x: 660, y: 780, s: 100 }, { x: -100, y: -100, s: 100 }, { x: 820, y: 540, s: 200 }, { x: -100, y: -100, s: 100 }, { x: 820, y: 540, s: 200 },
-        { x: -100, y: -100, s: 100 }, { x: 830, y: 40, s: 800 }, { x: -100, y: -100, s: 100 }, { x: 1080, y: 630, s: 260 }, { x: -100, y: -100, s: 100 },
-        { x: 0, y: 0, s: 2000 }, { x: -100, y: -100, s: 100 }, { x: 520, y: 530, s: 200 }, { x: -100, y: -100, s: 100 }, { x: 1110, y: 550, s: 150 },
-        { x: -100, y: -100, s: 100 }, { x: 630, y: 160, s: 1000 }];
-    //  1-90, 90-270, 272-362, 362-460, 460-550, 550-1100, 1100-1190, 1190-2000, 2000-2090, 2090-2300, 2300-2390, 2390-3000, 3000-3090, 3090-3650, 3650-3740, 3740-4200, 4200-4290, 
-    //   bt1cy  bt2cy    bt3ev   bt4ev    t5ev     t6ev       bt7ev      bt8ev     bt9ev      bt10ev     bt11ev     bt12ev     bt13ev     bt14ev    bt15ev     bt16ev      bt17ev
+        "T13.mp4", "T14.mp4", "T15.mp4", "T16.mp4", "T17.mp4", "T18.mp4", "T19.mp4", "T20.mp4", "T21.mp4", "T22.mp4", "T23.mp4", "T24.mp4",
+        "T25.mp4", "T26.mp4", "T27.mp4", "T28.mp4", "T29.mp4", "T30.mp4", "T31.mp4", "T32.mp4", "T33.mp4"];
+    var hitboxLocation = [{ x: 420, y: 500, s: 100 }, { x: -100, y: -100, s: 100 }, { x: 540, y: 360, s: 150 }, { x: -100, y: -100, s: 100 }, { x: 520, y: 340, s: 200 },
+        { x: -100, y: -100, s: 100 }, { x: 630, y: 0, s: 800 }, { x: -100, y: -100, s: 100 }, { x: 660, y: 400, s: 260 }, { x: -100, y: -100, s: 100 },
+        { x: 0, y: 0, s: 1300 }, { x: -100, y: -100, s: 100 }, { x: 320, y: 330, s: 150 }, { x: -100, y: -100, s: 100 }, { x: 730, y: 380, s: 150 },
+        { x: -100, y: -100, s: 100 }, { x: 400, y: 130, s: 500 }, { x: -100, y: -100, s: 100 }, { x: 590, y: 450, s: 200 }, { x: -100, y: -100, s: 100 },
+        { x: 550, y: 230, s: 250 }, { x: -100, y: -100, s: 100 }, { x: 700, y: 230, s: 250 }, { x: -100, y: -100, s: 100 }, { x: 550, y: 230, s: 250 },
+        { x: -100, y: -100, s: 100 }, { x: 550, y: 230, s: 250 }, { x: -100, y: -100, s: 100 }, { x: 550, y: 230, s: 250 }, { x: -100, y: -100, s: 100 },
+        { x: 550, y: 230, s: 250 }, { x: -100, y: -100, s: 100 }, { x: 0, y: 0, s: 1300 }];
+    //  1-90, 90-270, 272-362, 362-460, 460-550, 550-1100, 1100-1190, 1190-2000,
+    //  lp1ev  lp2ev   lp3cy    lp4ev    lp5ev     lp6ev     lp7ev     lp8ev     
+    //  2000-2090, 2090-2300, 2300-2390, 2390-3000, 3000-3090, 3090-3650, 3650-3740, 3740-4200, 4200-4290, 4290-4380, 4380-4470, 4470-5000, 5000-5087, 5087-5400, 5400-5490
+    //    lp9ev      lp10ev     lp11ev     lp12ev     lp13ev     lp14ev    lp15ev     lp16ev      lp17ev     lp18ev    lp19ev      lp20ev     lp21ev    lp22ev      lp23ev
+    //  5490-5580, 5580-5670, 5670-6000, 6000-6090, 6090-6420, 6420-6510, 6510-6600, 6600-6690, 6690-7200, 7200-7290
+    //    lp24ev    lp25ev     xlp26ev    lp27ev    xlp28ev    lp29ev     lp30ev     lp31ev    xlp32ev       lp33ev
     var hitbox = [hitboxLocation[0].x, hitboxLocation[0].y, hitboxLocation[0].s, hitboxLocation[0].s];
     var nextVideo = false;
     var doOneThanSkip = false;
-    var scaleFactor = 4.55;
+    var scaleFactor = 6.75;
     var debugMode = false;
     var interval;
     var x;
@@ -29,8 +37,8 @@ function hdlLoad() {
     debugbtn.addEventListener("click", toggleDebugMode);
     canvas.addEventListener("click", function (e) { return action(getCursorPosition(canvas, e)); });
     canvas.addEventListener("mousemove", function (e) { return hoverVis(getCursorPosition(canvas, e)); });
-    canvas.width = 1920;
-    canvas.height = 1080;
+    canvas.width = 1280;
+    canvas.height = 720;
     canvas.style.width = canvas.width * (scaleFactor / 10) + "px";
     function action(vector) {
         console.log(vector);
