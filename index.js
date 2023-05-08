6f601ae65f105d0701f058ff509de850d26dc393
Woschny <92034289+Woschny@users.noreply.github.com>
2023-05-08 20:54:42 +0200

    check v3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", hdlLoad);
-console.log("v2");
+console.log("v3");
 function hdlLoad() {
     localStorage.clear();
     var canvas = document.getElementById("canvas");
@@ -8,23 +8,23 @@ function hdlLoad() {
     var monImg = document.getElementById("MoniImg");
     var ctx = canvas.getContext("2d");
     var videoInt = 0;
-    var video = document.getElementById("video");
-    var sourceString = ["T1.mp4", "T2.mp4", "T3.mp4", "T4.mp4", "T5.mp4", "T6.mp4", "T7.mp4", "T8.mp4", "T9.mp4", "T10.mp4", "T11.mp4", "T12.mp4",
-        "T13.mp4", "T14.mp4", "T15.mp4", "T16.mp4", "T17.mp4", "T18.mp4", "T19.mp4", "T20.mp4", "T21.mp4", "T22.mp4", "T23.mp4", "T24.mp4",
-        "T25.mp4", "T26.mp4", "T27.mp4", "T28.mp4", "T29.mp4", "T30.mp4", "T31.mp4", "T32.mp4", "T33.mp4"];
+    var video = document.getElementById("T1");
+    var sourceString = ["T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9", "T10", "T11", "T12",
+        "T13", "T14", "T15", "T16", "T17", "T18", "T19", "T20", "T21", "T22", "T23", "T24",
+        "T25", "T26", "T27", "T28", "T29", "T30", "T31", "T32", "T33"];
     var hitboxLocation = [{ x: 420, y: 500, s: 100 }, { x: -100, y: -100, s: 100 }, { x: 540, y: 360, s: 150 }, { x: -100, y: -100, s: 100 }, { x: 520, y: 340, s: 200 },
         { x: -100, y: -100, s: 100 }, { x: 630, y: 0, s: 800 }, { x: -100, y: -100, s: 100 }, { x: 660, y: 400, s: 260 }, { x: -100, y: -100, s: 100 },
         { x: 0, y: 0, s: 1300 }, { x: -100, y: -100, s: 100 }, { x: 320, y: 330, s: 150 }, { x: -100, y: -100, s: 100 }, { x: 730, y: 380, s: 150 },
         { x: -100, y: -100, s: 100 }, { x: 400, y: 130, s: 500 }, { x: -100, y: -100, s: 100 }, { x: 590, y: 450, s: 200 }, { x: -100, y: -100, s: 100 },
-        { x: 550, y: 230, s: 250 }, { x: -100, y: -100, s: 100 }, { x: 700, y: 230, s: 250 }, { x: -100, y: -100, s: 100 }, { x: 550, y: 230, s: 250 },
-        { x: -100, y: -100, s: 100 }, { x: 550, y: 230, s: 250 }, { x: -100, y: -100, s: 100 }, { x: 550, y: 230, s: 250 }, { x: -100, y: -100, s: 100 },
+        { x: 550, y: 230, s: 250 }, { x: -100, y: -100, s: 100 }, { x: 400, y: 150, s: 250 }, { x: -100, y: -100, s: 100 }, { x: 550, y: 230, s: 250 },
+        { x: -100, y: -100, s: 100 }, { x: 300, y: 350, s: 250 }, { x: -100, y: -100, s: 100 }, { x: 550, y: 230, s: 250 }, { x: -100, y: -100, s: 100 },
         { x: 550, y: 230, s: 250 }, { x: -100, y: -100, s: 100 }, { x: 0, y: 0, s: 1300 }];
     //  1-90, 90-270, 272-362, 362-460, 460-550, 550-1100, 1100-1190, 1190-2000,
     //  lp1ev  lp2ev   lp3cy    lp4ev    lp5ev     lp6ev     lp7ev     lp8ev     
     //  2000-2090, 2090-2300, 2300-2390, 2390-3000, 3000-3090, 3090-3650, 3650-3740, 3740-4200, 4200-4290, 4290-4380, 4380-4470, 4470-5000, 5000-5087, 5087-5400, 5400-5490
     //    lp9ev      lp10ev     lp11ev     lp12ev     lp13ev     lp14ev    lp15ev     lp16ev      lp17ev     lp18ev    lp19ev      lp20ev     lp21ev    lp22ev      lp23ev
     //  5490-5580, 5580-5670, 5670-6000, 6000-6090, 6090-6420, 6420-6510, 6510-6600, 6600-6690, 6690-7200, 7200-7290
-    //    lp24ev    lp25ev     xlp26ev    lp27ev    xlp28ev    lp29ev     lp30ev     lp31ev    xlp32ev       lp33ev
+    //    lp24ev    lp25ev      lp26ev    lp27ev     lp28ev    lp29ev     lp30ev     lp31ev    xlp32ev       lp33ev
     var hitbox = [hitboxLocation[0].x, hitboxLocation[0].y, hitboxLocation[0].s, hitboxLocation[0].s];
     var nextVideo = false;
     var doOneThanSkip = false;
@@ -63,15 +63,14 @@ function hdlLoad() {
         }
     }
     function startVideo() {
+        console.log(video);
         video.play();
+        LoopStart();
     }
-    function pauseVideo() {
-        video.pause();
-    }
-    video.addEventListener("play", function () {
-        var $this = this;
+    function LoopStart() {
+        var $this = video;
         (function loop() {
-            if (!$this.paused && !$this.ended) {
+            if (!$this.ended) {
                 ctx.drawImage($this, 0, 0);
                 if (debugMode == true) {
                     ctx.fillStyle = "rgba(255, 0, 0, 0.3)";
@@ -80,12 +79,11 @@ function hdlLoad() {
                 setTimeout(loop, 1000 / 24);
             }
             else {
-                pauseVideo();
                 if (doOneThanSkip == true) {
                     videoInt++;
                     console.log(videoInt, sourceString.length);
                     if (videoInt < sourceString.length) {
-                        video.src = "./Assets/" + sourceString[videoInt];
+                        video = document.getElementById(sourceString[videoInt] + "");
                         hitbox[0] = hitboxLocation[videoInt].x;
                         hitbox[1] = hitboxLocation[videoInt].y;
                         hitbox[2] = hitboxLocation[videoInt].s;
@@ -100,14 +98,13 @@ function hdlLoad() {
                         videoInt++;
                         console.log(videoInt, sourceString.length);
                         if (videoInt < sourceString.length) {
-                            video.src = "./Assets/" + sourceString[videoInt];
+                            video = document.getElementById(sourceString[videoInt] + "");
                             hitbox[0] = hitboxLocation[videoInt].x;
                             hitbox[1] = hitboxLocation[videoInt].y;
                             hitbox[2] = hitboxLocation[videoInt].s;
                             hitbox[3] = hitbox[2];
                         }
                         hoverVis([x, y]);
-                        startVideo();
                         nextVideo = false;
                         doOneThanSkip = true;
                     }
@@ -115,7 +112,8 @@ function hdlLoad() {
                 startVideo();
             }
         })();
-    });
+    }
+    ;
     function getCursorPosition(canvas, event) {
         var rect = canvas.getBoundingClientRect();
         x = (event.clientX - rect.left) * (10 / scaleFactor);
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 window.addEventListener("load", hdlLoad);
-console.log("v2");
+console.log("v3");
 
 function hdlLoad(): void {
 localStorage.clear();
@@ -9,16 +9,16 @@ const debugbtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("
 const monImg: HTMLImageElement = <HTMLImageElement>document.getElementById("MoniImg");
 const ctx: CanvasRenderingContext2D    = canvas.getContext("2d");
 let videoInt: number = 0;
-let video: HTMLVideoElement  = <HTMLVideoElement>document.getElementById("video");
-let sourceString: string[] = [  "T1.mp4", "T2.mp4", "T3.mp4", "T4.mp4", "T5.mp4", "T6.mp4", "T7.mp4", "T8.mp4", "T9.mp4", "T10.mp4", "T11.mp4", "T12.mp4",
-                                "T13.mp4", "T14.mp4", "T15.mp4", "T16.mp4", "T17.mp4", "T18.mp4", "T19.mp4", "T20.mp4", "T21.mp4", "T22.mp4", "T23.mp4", "T24.mp4",
-                                "T25.mp4", "T26.mp4", "T27.mp4", "T28.mp4", "T29.mp4", "T30.mp4", "T31.mp4", "T32.mp4", "T33.mp4"];
+let video: HTMLVideoElement  = <HTMLVideoElement>document.getElementById("T1");
+let sourceString: string[] = [  "T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9", "T10", "T11", "T12",
+                                "T13", "T14", "T15", "T16", "T17", "T18", "T19", "T20", "T21", "T22", "T23", "T24",
+                                "T25", "T26", "T27", "T28", "T29", "T30", "T31", "T32", "T33"];
 let hitboxLocation: Vector[] = [{x: 420, y: 500, s: 100}, {x: -100, y: -100, s: 100}, {x: 540, y: 360, s: 150}, {x: -100, y: -100, s: 100}, {x: 520, y: 340, s: 200}, 
                                 {x: -100, y: -100, s: 100}, {x: 630, y: 0, s: 800}, {x: -100, y: -100, s: 100}, {x: 660, y: 400, s: 260}, {x: -100, y: -100, s: 100},
                                 {x: 0, y: 0, s: 1300}, {x: -100, y: -100, s: 100}, {x: 320, y: 330, s: 150}, {x: -100, y: -100, s: 100}, {x: 730, y: 380, s: 150},
                                 {x: -100, y: -100, s: 100}, {x: 400, y: 130, s: 500}, {x: -100, y: -100, s: 100}, {x: 590, y: 450, s: 200}, {x: -100, y: -100, s: 100},
-                                {x: 550, y: 230, s: 250}, {x: -100, y: -100, s: 100}, {x: 700, y: 230, s: 250}, {x: -100, y: -100, s: 100}, {x: 550, y: 230, s: 250}, 
-                                {x: -100, y: -100, s: 100}, {x: 550, y: 230, s: 250}, {x: -100, y: -100, s: 100}, {x: 550, y: 230, s: 250}, {x: -100, y: -100, s: 100},
+                                {x: 550, y: 230, s: 250}, {x: -100, y: -100, s: 100}, {x: 400, y: 150, s: 250}, {x: -100, y: -100, s: 100}, {x: 550, y: 230, s: 250}, 
+                                {x: -100, y: -100, s: 100}, {x: 300, y: 350, s: 250}, {x: -100, y: -100, s: 100}, {x: 550, y: 230, s: 250}, {x: -100, y: -100, s: 100},
                                 {x: 550, y: 230, s: 250}, {x: -100, y: -100, s: 100}, {x: 0, y: 0, s: 1300}];
 //  1-90, 90-270, 272-362, 362-460, 460-550, 550-1100, 1100-1190, 1190-2000,
 //  lp1ev  lp2ev   lp3cy    lp4ev    lp5ev     lp6ev     lp7ev     lp8ev     
@@ -27,7 +27,7 @@ let hitboxLocation: Vector[] = [{x: 420, y: 500, s: 100}, {x: -100, y: -100, s: 
 //    lp9ev      lp10ev     lp11ev     lp12ev     lp13ev     lp14ev    lp15ev     lp16ev      lp17ev     lp18ev    lp19ev      lp20ev     lp21ev    lp22ev      lp23ev
 
 //  5490-5580, 5580-5670, 5670-6000, 6000-6090, 6090-6420, 6420-6510, 6510-6600, 6600-6690, 6690-7200, 7200-7290
-//    lp24ev    lp25ev     xlp26ev    lp27ev    xlp28ev    lp29ev     lp30ev     lp31ev    xlp32ev       lp33ev
+//    lp24ev    lp25ev      lp26ev    lp27ev     lp28ev    lp29ev     lp30ev     lp31ev    xlp32ev       lp33ev
 let hitbox: number[] = [hitboxLocation[0].x, hitboxLocation[0].y, hitboxLocation[0].s, hitboxLocation[0].s];
 interface Vector {
     x: number;
@@ -79,17 +79,15 @@ function hoverVis(vector: number[]): void {
 }
 
 function startVideo(): void {
+        console.log(video);
         video.play();
-
-}
-function pauseVideo(): void {
-    video.pause();
+        LoopStart();         
 }
 
-video.addEventListener("play", function (): void {
-    let $this: HTMLVideoElement = this;
+function LoopStart(): void {
+    let $this: HTMLVideoElement = video;
     (function loop(): void {
-        if (!$this.paused && !$this.ended) {
+        if (!$this.ended) {
             ctx.drawImage($this, 0, 0);
             if(debugMode == true) {
             ctx.fillStyle = "rgba(255, 0, 0, 0.3)";
@@ -98,13 +96,11 @@ video.addEventListener("play", function (): void {
             setTimeout(loop, 1000 / 24);
         }
         else {
-            pauseVideo();
             if (doOneThanSkip == true){
                 videoInt++;
                 console.log(videoInt, sourceString.length);
-                
                 if(videoInt < sourceString.length) {
-                video.src = "./Assets/" + sourceString[videoInt];
+                video = <HTMLVideoElement>document.getElementById(sourceString[videoInt] + "");
                 hitbox[0] = hitboxLocation[videoInt].x;
                 hitbox[1] = hitboxLocation[videoInt].y;
                 hitbox[2] = hitboxLocation[videoInt].s;
@@ -119,14 +115,13 @@ video.addEventListener("play", function (): void {
                 videoInt ++;
                 console.log(videoInt, sourceString.length);
                 if(videoInt < sourceString.length) {
-                video.src = "./Assets/" + sourceString[videoInt];
+                video = <HTMLVideoElement>document.getElementById(sourceString[videoInt] + "");
                 hitbox[0] = hitboxLocation[videoInt].x;
                 hitbox[1] = hitboxLocation[videoInt].y;
                 hitbox[2] = hitboxLocation[videoInt].s;
                 hitbox[3] = hitbox[2];
                 }
                 hoverVis([x,y]);
-                startVideo();
                 nextVideo = false;
                 doOneThanSkip = true;
                 }
@@ -135,7 +130,7 @@ video.addEventListener("play", function (): void {
         }
     })();
 
-});
+};
 
 function getCursorPosition(canvas: HTMLCanvasElement, event: MouseEvent): number[] {
     const rect: ClientRect = canvas.getBoundingClientRect();
