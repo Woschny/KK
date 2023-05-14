bc57db3ff1a2c01c7ec607944ebf21048102617c
Woschny <92034289+Woschny@users.noreply.github.com>
2023-05-14 19:15:36 +0200

    text

--- index.ts
+++ index.ts
@@ -89,9 +89,6 @@ function LoopStart(): void {
     (function loop(): void {
         if (!$this.ended) {
             ctx.drawImage($this, 0, 0, canvas.width, canvas.height);
-            if(doOneThanSkip == true){
-                ctx.drawImage(TextImg, 0, 0, canvas.width, canvas.height);
-            }
             if(debugMode == true) {
             ctx.fillStyle = "rgba(255, 0, 0, 0.3)";
             ctx.fillRect(hitbox[0], hitbox[1], hitbox[2], hitbox[3]);
